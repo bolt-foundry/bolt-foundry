5d8f109dac05cf72afc9c3eefa833810e09c8b8a
Justin Carter <github@justicart.com>
2024-04-24 01:41:51 +0000

    add reels chrome

--- packages/client/components/MarketingIGHscroll.tsx
+++ packages/client/components/MarketingIGHscroll.tsx
@@ -1,9 +1,13 @@
 import { React } from "deps.ts";
+import Icon from "packages/bfDs/Icon.tsx";
 
 const igVideos = [
   {
     account: "blueridgecomedy",
     igHash: "C4xojmrq3VT",
+    caption: "Condom anxiety w/ @joesnotjoking #comedy #standupcomedy #funny",
+    likes: 38,
+    comments: 0,
   },
   {
     account: "blueridgecomedy",
@@ -52,10 +56,44 @@ export function MarketingIGHscroll() {
             <div className="hscroll-video" key={index}>
               <video
                 ref={videoRefs[index]}
-                src={`https://bf-static-assets.s3.amazonaws.com/videos/ig/${video.account}_${video.igHash}.mp4`}
+                src={`https://bf-static-assets.s3.amazonaws.com/ig/${video.account}_${video.igHash}.mp4`}
               >
                 Your browser does not support the video tag.
               </video>
+              <div className="hscroll-video-overlay">
+                <div className="hscroll-reels">Reels</div>
+                <div className="hscroll-ig">
+                  <Icon name="brand-instagram" color="white" />
+                </div>
+                <div className="hscroll-camera"></div>
+                <div className="hscroll-post">
+                  <div className="hscroll-post-author">
+                    <div
+                      className="hscroll-post-picture"
+                      style={{
+                        background:
+                          `url('https://bf-static-assets.s3.amazonaws.com/ig/${video.account}.jpg') center/cover no-repeat`,
+                      }}
+                    />
+                    <div className="hscroll-post-username">
+                      @{video.account}
+                    </div>
+                  </div>
+                  <div className="hscroll-post-text">
+                    {video.caption ?? ""}
+                  </div>
+                </div>
+                <div className="hscroll-chrome">
+                  <div className="hscroll-chrome-likes">
+                    {video.likes ?? 0}
+                  </div>
+                  <div className="hscroll-chrome-comments">
+                    {video.comments ?? 0}
+                  </div>
+                  <div className="hscroll-chrome-share" />
+                  <div className="hscroll-chrome-kebab" />
+                </div>
+              </div>
             </div>
           );
         })}
