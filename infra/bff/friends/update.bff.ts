e10f45fbdba655fd1428d52e12a496be32d9b7b8
Randall Bennett <randall@boltfoundry.com>
2024-05-02 14:57:54 +0000

    Create a deploy script
    Summary:
    Creates a deploy script which overwrites all changes and resets to the main branch.
    
    Test Plan:

--- infra/bff/friends/update.bff.ts
+++ infra/bff/friends/update.bff.ts
@@ -1,12 +1,36 @@
-import { runShellCommand } from "infra/bff/shellBase.ts";
+import {
+  runShellCommand,
+  runShellCommandWithOutput,
+} from "infra/bff/shellBase.ts";
 import { register } from "infra/bff/mod.ts";
 
 register(
   "update",
-  "Pulls changes down",
+  "Pulls changes down.",
   async () => {
-    await runShellCommand(["sl", "pull"]);
-    await runShellCommand(["sl", "goto", "main"]);
+    const ghToken = await runShellCommandWithOutput([
+      "replitbf-git-askpass",
+      "Password for 'https://token@github.com': ",
+    ]);
+    await runShellCommand(["sl", "pull"], { GH_TOKEN: ghToken });
+    await runShellCommand(["sl", "goto", "main"], { GH_TOKEN: ghToken });
+    return 0;
+  },
+);
+
+register(
+  "deploy",
+  "Pulls changes down. WILL OVERWRITE ANY CURRENT CHANGES.",
+  async () => {
+    console.log(
+      "This command will overwrite any changes you have made to the current branch. ctrl + c now. You have 5 seconds",)
+    await runShellCommand(['sleep', '5']);
+    const ghToken = await runShellCommandWithOutput([
+      "replit-git-askpass",
+      "Password for 'https://token@github.com': ",
+    ]);
+    await runShellCommand(["sl", "pull"], { GH_TOKEN: ghToken });
+    await runShellCommand(["sl", "goto", "main", "--clean"], { GH_TOKEN: ghToken });
     return 0;
   },
 );
