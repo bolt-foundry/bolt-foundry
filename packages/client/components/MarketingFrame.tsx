509a192df3a1de615c68343c302f0317da028208
Randall Bennett <randall@boltfoundry.com>
2024-05-07 20:14:41 +0000

    Changes the top login button temporarily
    Summary:
    
    Test Plan:

--- packages/client/components/MarketingFrame.tsx
+++ packages/client/components/MarketingFrame.tsx
@@ -98,7 +98,7 @@ export function MarketingFrame(
         </div>
         {showLoginLink && (
           <div className="nav-links mobile-hide" style={styles.rightSide}>
-            <Button href="/login" iconLeft="computer"/>
+            <Button href="/login" iconLeft="checkCircle"/>
           </div>
         )}
         <div
