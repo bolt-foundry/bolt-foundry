70ad416e1561ec8de158b5cb2c9e49bc9e1f8d71
Colby Baker <colby@boltfoundry.com>
2024-06-07 01:35:08 +0000

    Colby Bootcampt project 1
    Summary:
    
    Test Plan:

---
+++ packages/client/pages/ColbysPage.tsx
@@ -0,0 +1,10 @@
+import { React } from "deps.ts";
+
+export function ColbysPage(): React.ReactElement {
+  return (
+    <div>
+      <h1>Hello my name is Colby</h1>
+      <a href="https://www.colbybaker.com">Check out my website!</a>
+    </div>
+  );
+}
