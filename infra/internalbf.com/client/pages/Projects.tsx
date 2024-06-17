e9a5654640940902b061896e06789e4c694bf4ba
George LeVitre <george@boltfoundry.com>
2024-06-17 18:01:25 +0000

    Built a product page
    Summary: built a simply product page. Can be accessed at /projects
    
    Test Plan:

---
+++ infra/internalbf.com/client/pages/Projects.tsx
@@ -0,0 +1,56 @@
+import { React } from "deps.ts";
+import { BfSymbol } from "packages/bfDs/static/BfSymbol.tsx";
+import { UserProfile } from "packages/client/components/UserProfile.tsx";
+import { Icon } from "packages/bfDs/Icon.tsx";
+import { useRouter } from "infra/internalbf.com/client/contexts/RouterContext.tsx";
+import { InternalProjectsList } from "infra/internalbf.com/client/components/InternalProjectsList.tsx";
+
+export function Projects() {
+  const { navigate } = useRouter();
+  return (
+    <div className="internalPage">
+      <div className="internalSidebar">
+        <div className="internalLogo">
+          <div style={{ height: 32 }}>
+            <BfSymbol
+              color="var(--backgroundIcon)"
+              color2="var(--textSecondary)"
+            />
+          </div>
+          <div>InternalBF</div>
+        </div>
+        <div className="internalTabs">
+          <div className="internalTab" onClick={() => navigate("/media")}>
+            <Icon name="clipping" />Media
+          </div>
+          <div className="internalTab">
+            <Icon name="subtitle" />Organizations
+          </div>
+          <div
+            className="internalTab selected"
+            onClick={() => navigate("/projects")}
+          >
+            <Icon name="pencil" color="var(--secondaryColor)" />Projects
+          </div>
+          <div className="internalTab">
+            <Icon name="settings" />System
+          </div>
+          <div className="internalTab">
+            <Icon name="computer" />Your stuff
+          </div>
+        </div>
+        <div style={{ position: "absolute", bottom: 0, margin: 20 }}>
+          <UserProfile />
+        </div>
+      </div>
+      <div className="internalMain">
+        <div className="internalMainHeader">
+          Projects
+        </div>
+        <div className="internalMainContent">
+          <InternalProjectsList />
+        </div>
+      </div>
+    </div>
+  );
+}
