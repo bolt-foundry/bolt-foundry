3cbf40262404e64ccb6b9258f5e6605a5c4f2555
Randall Bennett <randall@boltfoundry.com>
2024-07-05 20:59:35 +0000

    The great cullening
    Summary:
    
    Culling all the things that aren't currently used. RIP my sweet princes.
    
    Test Plan:

--- infra/__generated__/_graphql_imports.ts
+++ infra/__generated__/_graphql_imports.ts
@@ -1,6 +1,12 @@
 import "infra/__generated__/LoginPageLogoutMutation.graphql.ts"
+<<<<<<< dest:   c2ae90456f57 - randallb: Ingest updates
 import "infra/__generated__/GoogleDriveFilePickerQuery.graphql.ts"
 import "infra/__generated__/GoogleDriveFilePickerLinkGoogleAccountMutation.graphql.ts"
 import "infra/__generated__/InternalBfPageCreateIngestionJobMutation.graphql.ts"
 import "infra/__generated__/LoginPageLoginWithGoogleMutation.graphql.ts"
-import "infra/__generated__/LoginPageCVQuery.graphql.ts"
+import "infra/__generated__/LoginPageCVQuery.graphql.ts"||||||| base:   659af271fc05 - randall: Change everything to go through boltf...
+import "infra/__generated__/LoginPageLoginWithGoogleMutation.graphql.ts"
+import "infra/__generated__/GoogleDriveFilePickerQuery.graphql.ts"
+import "infra/__generated__/GoogleDriveFilePickerLinkGoogleAccountMutation.graphql.ts"=======
+import "infra/__generated__/LoginPageLoginWithGoogleMutation.graphql.ts"
+import "infra/__generated__/LoginPageCVQuery.graphql.ts">>>>>>> source: 9f325fba496b - randall: The great cullening
--- packages/__generated__/_graphql_imports.ts
+++ packages/__generated__/_graphql_imports.ts
@@ -1,3 +1,4 @@
+<<<<<<< dest:   c2ae90456f57 - randallb: Ingest updates
 import "packages/__generated__/ProjectView_containerProject.graphql.ts"
 import "packages/__generated__/ContactUsSubmitFormMutation.graphql.ts"
 import "packages/__generated__/ProjectNewCreateNewProjectMutation.graphql.ts"
@@ -5,6 +6,14 @@ import "packages/__generated__/ProjectNewUploadFileMutation.graphql.ts"
 import "packages/__generated__/ProjectPageContainerProjectQuery.graphql.ts"
 import "packages/__generated__/BfContainerProjectResolverOpurlResolver.graphql.ts"
 import "packages/__generated__/ProjectNewCreateBffsFileMutation.graphql.ts"
+||||||| base:   659af271fc05 - randall: Change everything to go through boltf...
+import "packages/__generated__/BfContainerProjectResolverOpurlResolver.graphql.ts"
+import "packages/__generated__/ProjectNewCreateBffsFileMutation.graphql.ts"
+import "packages/__generated__/ProjectView_containerProject.graphql.ts"
+import "packages/__generated__/LoginFormLoginWithGoogleMutation.graphql.ts"
+=======
+import "packages/__generated__/ContactUsSubmitFormMutation.graphql.ts"
+>>>>>>> source: 9f325fba496b - randall: The great cullening
 import "packages/__generated__/LoginPageCVQuery.graphql.ts"
 import "packages/__generated__/LoginFormLoginWithGoogleMutation.graphql.ts"
 import "packages/__generated__/LoginPageLogoutMutation.graphql.ts"
