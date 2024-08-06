f80786a13c95a33a554f6d2aa2c0f167f289af17
George LeVitre <george@boltfoundry.com>
2024-08-06 14:20:11 +0000

    Beautify UI
    Summary: Built initial UI for new product with justin. Heavily based on the old word
    
    Test Plan:

--- packages/client/components/App.tsx
+++ packages/client/components/App.tsx
@@ -11,12 +11,13 @@ import { LoginPage } from "packages/client/pages/LoginPage.tsx";
 import { Demo } from "packages/bfDs/Demo.tsx";
 import { ContactUs } from "packages/client/components/ContactUs.tsx";
 import { ComingSoonPage } from "packages/client/pages/ComingSoonPage.tsx";
-
+import { ClipSearchPage } from "packages/client/pages/ClipSearchPage.tsx";
 export const routes = new Map([
   ["/", { Component: ComingSoonPage, allowLoggedOut: true }],
   ["/login", { Component: LoginPage, allowLoggedOut: true }],
   ["/ui", { Component: Demo, allowLoggedOut: true }],
   ["/contact-us", { Component: ContactUs, allowLoggedOut: true }],
+  ["/clip-search", { Component: ClipSearchPage, allowLoggedOut: true }],
 ]);
 
 export function App() {
