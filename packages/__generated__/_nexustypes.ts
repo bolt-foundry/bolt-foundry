1246d6bc15e0aca7c4dd5bfed6ee402d4dc6bbd4
Justin Carter <justin@boltfoundry.com>
2024-08-06 19:55:18 +0000

    [2/3] add update to model and filename to transcript
    Summary: Add update to Model
    Add filename to transcript and updateTranscript
    
    Test Plan:

--- packages/__generated__/_nexustypes.ts
+++ packages/__generated__/_nexustypes.ts
@@ -118,6 +118,7 @@ export interface NexusGenObjects {
     name?: string | null; // String
   }
   BfTranscript: { // root type
+    filename?: string | null; // String
     transcript?: string | null; // String
   }
   Mutation: {};
@@ -207,6 +208,7 @@ export interface NexusGenFieldTypes {
     name: string | null; // String
   }
   BfTranscript: { // field return type
+    filename: string | null; // String
     id: string; // ID!
     transcript: string | null; // String
   }
@@ -218,6 +220,7 @@ export interface NexusGenFieldTypes {
     readTextFile: string | null; // String
     submitContactForm: NexusGenRootTypes['SubmitContactFormPayload'] | null; // SubmitContactFormPayload
     switchAccount: NexusGenRootTypes['BfCurrentViewerAccessToken'] | null; // BfCurrentViewerAccessToken
+    updateTranscript: NexusGenRootTypes['BfTranscript'] | null; // BfTranscript
     upsertClip: NexusGenRootTypes['BfClip'] | null; // BfClip
   }
   PageInfo: { // field return type
@@ -306,6 +309,7 @@ export interface NexusGenFieldTypeNames {
     name: 'String'
   }
   BfTranscript: { // field return type name
+    filename: 'String'
     id: 'ID'
     transcript: 'String'
   }
@@ -317,6 +321,7 @@ export interface NexusGenFieldTypeNames {
     readTextFile: 'String'
     submitContactForm: 'SubmitContactFormPayload'
     switchAccount: 'BfCurrentViewerAccessToken'
+    updateTranscript: 'BfTranscript'
     upsertClip: 'BfClip'
   }
   PageInfo: { // field return type name
@@ -363,6 +368,7 @@ export interface NexusGenArgTypes {
   }
   Mutation: {
     createTranscript: { // args
+      filename: string; // String!
       transcript: string; // String!
     }
     deleteTranscript: { // args
@@ -379,6 +385,11 @@ export interface NexusGenArgTypes {
     }
     switchAccount: { // args
       accountId: string; // ID!
+    }
+    updateTranscript: { // args
+      filename?: string | null; // String
+      id: string; // String!
+      transcript?: string | null; // String
     }
     upsertClip: { // args
       file: NexusGenScalars['File']; // File!
