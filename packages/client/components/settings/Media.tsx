cfec35d9972bfa7120c92d9b6d861873435b5295
George LeVitre <george@boltfoundry.com>
2024-08-12 21:09:45 +0000

    Added styling and dynamic rendering
    Summary:
    
    Test Plan:

--- packages/client/components/settings/Media.tsx
+++ packages/client/components/settings/Media.tsx
@@ -69,11 +69,13 @@ export function Media({ settings$key }: Props) {
   ];
 
   return (
+    <div className="cs-main">
     <div className="cs-page-content">
       <div className="cs-page-section">
         <div className="cs-page-section-title">Media</div>
         <Table columns={columns} data={tableData} />
       </div>
+    </div>
     </div>
   );
 }
