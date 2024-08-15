26967f8bec4453e750dae39c624b7e716559cfd2
Randall Bennett <randall@boltfoundry.com>
2024-08-15 17:30:18 +0000

    [WIP for colby]
    Summary:
    
    Test Plan:

---
+++ lib/notionApi.ts
@@ -0,0 +1,12 @@
+// cms: https://www.notion.so/boltfoundry/1d6117a9a86441f1b339e96346bbf0e1?v=887423b5acf24d93b501bd3975b0c11e
+// packages/graphql/types/GraphQLBlog.ts
+// packages/client/pages/BlogPage.tsx
+
+const NOTION_BLOG_ID = "1d6117a9a86441f1b339e96346bbf0e1";
+export async function getBlogPostsFromNotion() {
+  return [];
+}
+
+export async function getBlogPostFromNotion(slug: string) {
+  return null;
+}
