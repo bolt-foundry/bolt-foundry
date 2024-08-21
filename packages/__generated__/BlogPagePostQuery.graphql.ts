d811d4b527bdf609380f1e0f8df7e71f7390cc47
Randall Bennett <randall@boltfoundry.com>
2024-08-21 21:21:48 +0000

    Add generated files to commit stack
    Summary:
    
    Test Plan:

--- packages/__generated__/BlogPagePostQuery.graphql.ts
+++ packages/__generated__/BlogPagePostQuery.graphql.ts
@@ -1,5 +1,5 @@
 /**
- * @generated SignedSource<<55a7d9ff19bac71291bc81f1683ff3f7>>
+ * @generated SignedSource<<b3b74b3b287943960c948df1dd6f88e7>>
  * @lightSyntaxTransform
  * @nogrep
  */
@@ -17,7 +17,13 @@ export type BlogPagePostQuery$data = {
     readonly blog: {
       readonly posts: {
         readonly nodes: ReadonlyArray<{
-          readonly content: string | null | undefined;
+          readonly content: ReadonlyArray<{
+            readonly id?: string | null | undefined;
+            readonly imgUrl?: string | null | undefined;
+            readonly rawText?: string | null | undefined;
+            readonly type: string | null | undefined;
+          } | null | undefined> | null | undefined;
+          readonly slug: string | null | undefined;
           readonly title: string | null | undefined;
         } | null | undefined> | null | undefined;
       } | null | undefined;
@@ -37,62 +43,81 @@ var v0 = [
     "name": "slug"
   }
 ],
-v1 = {
+v1 = [
+  {
+    "kind": "Literal",
+    "name": "first",
+    "value": 10
+  },
+  {
+    "kind": "Variable",
+    "name": "slug",
+    "variableName": "slug"
+  }
+],
+v2 = {
   "alias": null,
   "args": null,
-  "concreteType": "Blog",
-  "kind": "LinkedField",
-  "name": "blog",
-  "plural": false,
+  "kind": "ScalarField",
+  "name": "slug",
+  "storageKey": null
+},
+v3 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "title",
+  "storageKey": null
+},
+v4 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "type",
+  "storageKey": null
+},
+v5 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "id",
+  "storageKey": null
+},
+v6 = {
+  "kind": "InlineFragment",
   "selections": [
+    (v5/*: any*/),
     {
       "alias": null,
-      "args": [
-        {
-          "kind": "Literal",
-          "name": "first",
-          "value": 1
-        },
-        {
-          "kind": "Variable",
-          "name": "slug",
-          "variableName": "slug"
-        }
-      ],
-      "concreteType": "BlogPostConnection",
-      "kind": "LinkedField",
-      "name": "posts",
-      "plural": false,
-      "selections": [
-        {
-          "alias": null,
-          "args": null,
-          "concreteType": "BlogPost",
-          "kind": "LinkedField",
-          "name": "nodes",
-          "plural": true,
-          "selections": [
-            {
-              "alias": null,
-              "args": null,
-              "kind": "ScalarField",
-              "name": "title",
-              "storageKey": null
-            },
-            {
-              "alias": null,
-              "args": null,
-              "kind": "ScalarField",
-              "name": "content",
-              "storageKey": null
-            }
-          ],
-          "storageKey": null
-        }
-      ],
+      "args": null,
+      "kind": "ScalarField",
+      "name": "imgUrl",
       "storageKey": null
     }
   ],
+  "type": "ImageBlock",
+  "abstractKey": null
+},
+v7 = {
+  "kind": "InlineFragment",
+  "selections": [
+    (v5/*: any*/),
+    {
+      "alias": null,
+      "args": null,
+      "kind": "ScalarField",
+      "name": "rawText",
+      "storageKey": null
+    }
+  ],
+  "type": "ParagraphBlock",
+  "abstractKey": null
+},
+v8 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "__typename",
   "storageKey": null
 };
 return {
@@ -110,7 +135,55 @@ return {
         "name": "currentViewer",
         "plural": false,
         "selections": [
-          (v1/*: any*/)
+          {
+            "alias": null,
+            "args": null,
+            "concreteType": "Blog",
+            "kind": "LinkedField",
+            "name": "blog",
+            "plural": false,
+            "selections": [
+              {
+                "alias": null,
+                "args": (v1/*: any*/),
+                "concreteType": "BlogPostConnection",
+                "kind": "LinkedField",
+                "name": "posts",
+                "plural": false,
+                "selections": [
+                  {
+                    "alias": null,
+                    "args": null,
+                    "concreteType": "BlogPost",
+                    "kind": "LinkedField",
+                    "name": "nodes",
+                    "plural": true,
+                    "selections": [
+                      (v2/*: any*/),
+                      (v3/*: any*/),
+                      {
+                        "alias": null,
+                        "args": null,
+                        "concreteType": null,
+                        "kind": "LinkedField",
+                        "name": "content",
+                        "plural": true,
+                        "selections": [
+                          (v4/*: any*/),
+                          (v6/*: any*/),
+                          (v7/*: any*/)
+                        ],
+                        "storageKey": null
+                      }
+                    ],
+                    "storageKey": null
+                  }
+                ],
+                "storageKey": null
+              }
+            ],
+            "storageKey": null
+          }
         ],
         "storageKey": null
       }
@@ -132,30 +205,73 @@ return {
         "name": "currentViewer",
         "plural": false,
         "selections": [
+          (v8/*: any*/),
           {
             "alias": null,
             "args": null,
-            "kind": "ScalarField",
-            "name": "__typename",
+            "concreteType": "Blog",
+            "kind": "LinkedField",
+            "name": "blog",
+            "plural": false,
+            "selections": [
+              {
+                "alias": null,
+                "args": (v1/*: any*/),
+                "concreteType": "BlogPostConnection",
+                "kind": "LinkedField",
+                "name": "posts",
+                "plural": false,
+                "selections": [
+                  {
+                    "alias": null,
+                    "args": null,
+                    "concreteType": "BlogPost",
+                    "kind": "LinkedField",
+                    "name": "nodes",
+                    "plural": true,
+                    "selections": [
+                      (v2/*: any*/),
+                      (v3/*: any*/),
+                      {
+                        "alias": null,
+                        "args": null,
+                        "concreteType": null,
+                        "kind": "LinkedField",
+                        "name": "content",
+                        "plural": true,
+                        "selections": [
+                          (v8/*: any*/),
+                          (v4/*: any*/),
+                          (v6/*: any*/),
+                          (v7/*: any*/)
+                        ],
+                        "storageKey": null
+                      }
+                    ],
+                    "storageKey": null
+                  }
+                ],
+                "storageKey": null
+              }
+            ],
             "storageKey": null
-          },
-          (v1/*: any*/)
+          }
         ],
         "storageKey": null
       }
     ]
   },
   "params": {
-    "cacheID": "47f05f72c02533ee5296159a3bb5c0cb",
+    "cacheID": "795bad76c719a3c4a5868e9ded933084",
     "id": null,
     "metadata": {},
     "name": "BlogPagePostQuery",
     "operationKind": "query",
-    "text": "query BlogPagePostQuery(\n  $slug: String!\n) {\n  currentViewer {\n    __typename\n    blog {\n      posts(slug: $slug, first: 1) {\n        nodes {\n          title\n          content\n        }\n      }\n    }\n  }\n}\n"
+    "text": "query BlogPagePostQuery(\n  $slug: String!\n) {\n  currentViewer {\n    __typename\n    blog {\n      posts(slug: $slug, first: 10) {\n        nodes {\n          slug\n          title\n          content {\n            __typename\n            type\n            ... on ImageBlock {\n              id\n              imgUrl\n              type\n            }\n            ... on ParagraphBlock {\n              id\n              rawText\n              type\n            }\n          }\n        }\n      }\n    }\n  }\n}\n"
   }
 };
 })();
 
-(node as any).hash = "17f7004f5008e3bc9f485b9b5e15c54a";
+(node as any).hash = "c5679e63cccfb941a7ef3530abe18a45";
 
 export default node;
