74ccda2aaf1d1cd1e0835e67d6bf8679e0dcb91c
Randall Bennett <randall@boltfoundry.com>
2024-08-21 20:47:18 +0000

    Basic start for GraphQl for the Blog
    Summary: Allows the currentUser.blog to return a list of posts as well as fetch the content for one or more of those posts. For now it is only compatible with paragraphs and images and only returns basic data.
    
    Test Plan: 🦧

--- packages/graphql/types/GraphQLBlog.ts
+++ packages/graphql/types/GraphQLBlog.ts
@@ -1,5 +1,11 @@
-import { objectType, stringArg } from "nexus";
+import { interfaceType, list, objectType, stringArg } from "nexus";
 import { connectionFromArray } from "packages/graphql/deps.ts";
+import {
+  BlogPostData,
+  getBlogPostsFromNotion,
+  getListOfContentForAPost,
+  NotionBlogPostContent,
+} from "lib/notionApi.ts";
 
 export const Blog = objectType({
   name: "Blog",
@@ -11,11 +17,11 @@ export const Blog = objectType({
         slug: stringArg(),
       },
       resolve: async (_, args, ctx) => {
-        return connectionFromArray([{
-          title: "lol",
-          slug: "lol",
-          content: "lol",
-        }], args);
+        let posts: BlogPostData[] = await getBlogPostsFromNotion();
+        if (args.slug) {
+          posts = posts.filter((post) => post.slug === args.slug);
+        }
+        return connectionFromArray(posts, args);
       },
     });
   },
@@ -24,8 +30,55 @@ export const Blog = objectType({
 export const BlogPost = objectType({
   name: "BlogPost",
   definition(t) {
+    t.nonNull.string("id");
     t.string("title");
     t.string("slug");
-    t.string("content");
+    t.list.field("content", {
+      type: "BlogPostContentBlock",
+      resolve: async (parent, args, ctx) => {
+        const content: NotionBlogPostContent[] = await getListOfContentForAPost(
+          parent.id,
+        );
+        return content; // Directly return the list
+      },
+    });
+  },
+});
+
+export const ParagraphBlock = objectType({
+  name: "ParagraphBlock",
+  definition(t) {
+    t.implements("BlogPostContentBlock");
+    t.string("id");
+    t.string("type");
+    t.string("rawText");
+  },
+});
+export const ImageBlock = objectType({
+  name: "ImageBlock",
+  definition(t) {
+    t.implements("BlogPostContentBlock");
+    t.string("id");
+    t.string("type");
+    t.string("imgUrl");
+  },
+});
+
+export const BlogPostContentBlock = interfaceType({
+  name: "BlogPostContentBlock",
+  definition(t) {
+    t.string("type");
+    t.string("id");
+  },
+  resolveType(item) {
+    switch (item.type) {
+      case "paragraph":
+        return "ParagraphBlock";
+      case "image":
+        return "ImageBlock";
+      default:
+        //todo add error logging
+        return null;
+    }
   },
 });
