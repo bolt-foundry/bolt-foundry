a008efb47ea84b7fee3104891b4c346718967dda
Randall Bennett <randall@boltfoundry.com>
2024-08-23 18:47:32 +0000

    CI
    Summary:
    
    Test Plan:

--- flake.nix
+++ flake.nix
@@ -3,78 +3,82 @@
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "nixpkgs/nixos-24.05";
     nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
+    nix-github-actions.url = "github:nix-community/nix-github-actions";
   };
+  inputs.nix-github-actions.inputs.nixpkgs.follows = "nixpkgs";
 
-  outputs = { self, nixpkgs, flake-utils, nixpkgs-unstable }:
+  outputs = { self, nixpkgs, flake-utils, nixpkgs-unstable, nix-github-actions }:
     let
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
 
       pkgsForSystem = system: nixpkgsSource:
+        import nixpkgsSource {
+          inherit system;
+          overlays = [ ];
+          config.allowUnfree = true;
+        };
+
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+
+      packageSetsForSystem = system:
         let
-          filteredSrc = builtins.filterSource
-            (path: type: baseName: ! (builtins.match ".*/\\.sl" baseName != null))
-            ./.;
-
-          overlayedNixpkgs = import nixpkgsSource {
-            inherit system filteredSrc;
-
-            overlays = [ ];
-            config.allowUnfree = true;
-          };
+          pkgs = pkgsForSystem system nixpkgs;
+          unstablePkgs = pkgsForSystem system nixpkgs-unstable;
         in
-        overlayedNixpkgs;
+        {
+          sharedPackages = with pkgs; [
+            unstablePkgs.deno
+            ffmpeg
+            yarn
+            nodejs_22
+          ];
+
+          defaultPackages = with pkgs; [
+            watchman
+            sapling
+            gh
+            jupyter
+          ];
+
+          devShellPackages = with pkgs; [
+            jq
+          ];
+
+          deployPackages = with pkgs; [
+          ];
+        };
+
+      packageSets = forAllSystems packageSetsForSystem;
 
     in
-    flake-utils.lib.eachDefaultSystem (
-      system:
+    flake-utils.lib.eachSystem systems (system:
       let
         pkgs = pkgsForSystem system nixpkgs;
-        unstablePkgs = pkgsForSystem system nixpkgs-unstable;
-
-        # Add to this to make it everywhere
-        # Be sure to run `bff nix` to build
-        sharedPackages = with pkgs; [
-          unstablePkgs.deno
-          ffmpeg
-          yarn
-          nodejs_22
-        ];
-
-        # Add to this to make it available in VS Code and most places on the system. The larger this is, the larger the deploy, so try to keep it slim.
-        # Be sure to run `bff nix` to build
-        defaultPackages = with pkgs; [
-          watchman
-          sapling
-          gh
-          jupyter
-        ];
-
-        # These are dev-only packages, used for building etc. They're only available where direnv is available, ie the replit shell.
-        # these packages show up with "direnv allow"
-        devShellPackages = with pkgs; [
-          jq
-        ];
-        
-
-        deployPackages = with pkgs; [
-        ];
+        sets = packageSets.${system};
       in
       rec {
-
-        # This creates a default environment for defaultPackage and nix profile install
         packages.default = pkgs.buildEnv {
           name = "defaultPackage";
-          paths = sharedPackages ++ defaultPackages;
+          paths = sets.sharedPackages ++ sets.defaultPackages;
         };
 
         packages.deploy = pkgs.buildEnv {
           name = "deploy";
-          paths = sharedPackages ++ deployPackages;
+          paths = sets.sharedPackages ++ sets.deployPackages;
         };
 
-        # `nix develop` and direnv
+        packages.hello = pkgs.hello;
+
         devShells.default = pkgs.mkShell {
-          nativeBuildInputs = sharedPackages ++ devShellPackages ++ defaultPackages;
+          nativeBuildInputs = sets.sharedPackages ++ sets.devShellPackages ++ sets.defaultPackages;
         };
       }
-    );
+      
+    ) // {
+      packageSets = packageSets;
+
+      githubActions = nix-github-actions.lib.mkGithubMatrix {
+        checks = nixpkgs.lib.getAttrs [ "x86_64-linux" "x86_64-darwin" ] self.checks;
+      };
+    };
 }
