aa237031885d8cc845cfe3d1354292bd9f605a0c
Randall Bennett <randall@boltfoundry.com>
2024-08-24 17:10:14 +0000

    build cleanup
    Summary:
    
    Test Plan:

--- infra/bff/extension/deps.ts
+++
@@ -1,1 +0,0 @@
-export * as replit from "https://esm.sh/@replit/extensions@1.8.0";
--- infra/build/build.ts
+++ infra/build/build.ts
@@ -3,14 +3,13 @@
 import { esbuild } from "infra/build/deps.ts";
 import { generateBluey } from "lib/generateBluey.ts";
 import { denoPlugin } from "infra/build/bffEsbuild.ts";
-import { workerList } from "infra/build/workerList.ts";
 import { getLogger } from "deps.ts";
 
 const logger = getLogger(import.meta);
 
 export async function build(
   buildOptions = {
-    // minify: true,
+    minify: true,
     sourcemap: "inline" as const,
     sourceRoot: `${Deno.cwd()}`,
   },
@@ -19,7 +18,6 @@ export async function build(
   await esbuild.build({
     bundle: true,
     entryPoints: [
-      ...workerList,
       "packages/client/Client.tsx",
       "infra/internalbf.com/client/Client.tsx",
       "aws/client/main.tsx",
@@ -39,55 +37,7 @@ export async function build(
   logger.info("Building complete.");
 }
 
-const BF_PATH = Deno.env.get("BF_PATH") ?? "/bf/.";
-let lastBuilt = 0;
-// @ts-expect-error #techdebt
-async function watchedBuild(buildOptions) {
-  const timeSinceLastBuild = Date.now() - lastBuilt;
-  const includedPaths = [
-    "packages",
-    "infra",
-    "resources",
-    "experimental",
-    "content",
-  ];
-  const watcher = Deno.watchFs(".", { recursive: true });
-  for await (const event of watcher) {
-    const { paths } = event;
-    for (const path of paths) {
-      if (
-        !includedPaths.some((p) => path.startsWith(`${BF_PATH}/./${p}`)) ||
-        timeSinceLastBuild < 1000
-      ) {
-        // console.log(
-        //   `Skipping build due to change in ${path} -- ${timeSinceLastBuild}ms since last build -- ${BF_PATH}`,
-        // );
-        continue;
-      }
-      // deno-lint-ignore no-console
-      console.log("Building due to change in", path);
-      try {
-        await build(buildOptions);
-      } catch (e) {
-        // deno-lint-ignore no-console
-        console.warn(generateBluey(`Biscuts... Failed to build: ${e.message}`));
-        lastBuilt = Date.now();
-        continue;
-      }
-
-      // deno-lint-ignore no-console
-      console.log(generateBluey("Hooray!! Rebuilt the client.") + "\n");
-      lastBuilt = Date.now();
-    }
-  }
-}
-
 if (import.meta.main) {
-  let buildOptions;
-  const isLocalDevserver = Deno.env.get("IS_LOCAL_DEVSERVER");
-  if (isLocalDevserver === "true") {
-    buildOptions = { minify: false, sourcemap: true };
-  }
   try {
     await Deno.mkdir("build", { recursive: true });
   } catch (e) {
@@ -96,15 +46,11 @@ if (import.meta.main) {
     }
   }
   try {
-    await build(buildOptions);
+    await build();
   } catch (e) {
     // deno-lint-ignore no-console
     console.warn(
       generateBluey(`Biscuts... Failed initial build: ${e.message}`),
     );
-  }
-
-  if (Deno.args.includes("--watch")) {
-    await watchedBuild(buildOptions);
   }
 }
