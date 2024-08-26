e4e801e6866c292c18f65cd63e6345ee4697803c
Randall Bennett <randall@boltfoundry.com>
2024-08-26 00:56:16 +0000

    Slimbuild!
    Summary:
    
    Test Plan:

--- infra/build/slimBuild.ts
+++ infra/build/slimBuild.ts
@@ -3,6 +3,7 @@
 import * as esbuild from "esbuild";
 import { getLogger } from "deps.ts";
 import { generateBluey } from "lib/generateBluey.ts";
+import { extractGraphqlTags, replaceTagsWithImports } from "infra/build/slimRelay.ts";
 
 const logger = getLogger(import.meta);
 
@@ -27,6 +28,33 @@ const denoFileResolver = {
   },
 };
 
+const slimRelay = {
+  name: "slim-relay",
+  setup(build: esbuild.PluginBuild) {
+    build.onLoad({ filter: /\.(ts|js|tsx|jsx)$/ }, async (args) => {
+      const source = await Deno.readTextFile(args.path);
+      const graphqlTags = extractGraphqlTags(source);
+      let contents = source;
+      if (graphqlTags.length > 0) {
+        const relativeLocation =
+          args.path.split(Deno.env.get("BF_PATH") ?? "")[1];
+        const rootDirectory = relativeLocation.split("/")[1];
+
+        contents = await replaceTagsWithImports(
+          source,
+          graphqlTags,
+          rootDirectory,
+        );
+      }
+      const ext = args.path.match(/[^.]+$/);
+      const loader = (ext ? ext[0] : "ts") as esbuild.Loader;
+
+      logger.trace(`Loading local module: ${args.path}`);
+      return { contents, loader };
+    });
+  },
+};
+
 export async function build(
   buildOptions = {
     minify: true,
@@ -39,12 +67,14 @@ export async function build(
     bundle: true,
     entryPoints: [
       "infra/internalbf.com/client/Client.tsx",
+      "packages/client/Client.tsx",
     ],
     write: true,
-    outdir: "build/infra/internalbf.com/client",
+    outdir: "build",
     plugins: [
       // denoPlugin,
       denoFileResolver,
+      slimRelay,
     ],
     format: "esm",
     treeShaking: true,
