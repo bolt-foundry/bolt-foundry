c60631fbc11ab6a38f914c592721a50417eafc9f
Justin Carter <justin@boltfoundry.com>
2024-08-27 14:14:39 +0000

    add createTargetNode
    Summary:
    
    Test Plan:

--- packages/bfDb/coreModels/BfNode.ts
+++ packages/bfDb/coreModels/BfNode.ts
@@ -1,5 +1,13 @@
 import { BfModel } from "packages/bfDb/classes/BfModel.ts";
-import { CreationMetadata } from "packages/bfDb/classes/BfBaseModelMetadata.ts";
+import {
+  BfBaseModelMetadata,
+  Constructor,
+  CreationMetadata,
+} from "packages/bfDb/classes/BfBaseModelMetadata.ts";
+import { BfCurrentViewer } from "packages/bfDb/classes/BfCurrentViewer.ts";
+import { BfEdge } from "packages/bfDb/coreModels/BfEdge.ts";
+import { getLogger } from "deps.ts";
+const logger = getLogger(import.meta);
 
 export type BfNodeRequiredProps = Record<string, unknown>;
 export type BfNodeOptionalProps = Record<string, unknown>;
@@ -14,4 +22,44 @@ export class BfNode<
   ChildOptionalProps,
   ChildCreationMetadata
 > {
+  public async createTargetNode<
+    TThis extends Constructor<
+      BfModel<TRequiredProps, TOptionalProps, TCreationMetadata>
+    >,
+    TRequiredProps,
+    TOptionalProps,
+    TCreationMetadata extends CreationMetadata,
+  >(
+    this: BfNode,
+    currentViewer: BfCurrentViewer,
+    TargetClass: typeof BfNode,
+    targetProps: TRequiredProps & Partial<TOptionalProps>,
+    targetCreationMetadata: TCreationMetadata,
+  ): Promise<
+    InstanceType<TThis> & BfBaseModelMetadata<TCreationMetadata>
+  > {
+    logger.debug("createTargetNode", {
+      currentViewer,
+      TargetClass,
+      targetProps,
+      targetCreationMetadata,
+    });
+    const targetModel = await TargetClass.create(
+      currentViewer,
+      targetProps,
+      targetCreationMetadata,
+    );
+    await BfEdge.createEdgeBetweenNodes(
+      currentViewer,
+      this,
+      targetModel,
+    );
+    logger.debug("created edge", {
+      sourceId: this.metadata.bfGid,
+      targetId: targetModel.metadata.bfGid,
+    });
+    return targetModel as unknown as
+      & InstanceType<TThis>
+      & BfBaseModelMetadata<TCreationMetadata>;
+  }
 }
