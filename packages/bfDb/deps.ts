9910af60a78a18086ea3b5bf2d098233789ade06
Randall Bennett <randall@boltfoundry.com>
2024-09-03 14:42:31 +0000

    Move to BYONM strategy
    Summary:
    
    https://github.com/denoland/deno/issues/23151
    
    Use byonm so we don't have to mess around with node modules otherwise.
    
    Test Plan:

--- packages/bfDb/deps.ts
+++ packages/bfDb/deps.ts
@@ -2,7 +2,6 @@ export * from "https://deno.land/x/djwt@v2.9.1/mod.ts";
 // @deno-types="npm:@replit/object-storage"
 import { Client } from "@replit/object-storage";
 import { getLogger } from "deps.ts";
-export { Readable } from "node:stream";
 const logger = getLogger(import.meta);
 const REPLIT_OBJECT_STORAGE_BUCKET_URL =
   "http://127.0.0.1:1106/object-storage/default-bucket";
