ddd5005c2ab1491ece27b85c56ae8ff36fa8eaa1
Randall Bennett <randall@boltfoundry.com>
2024-09-06 20:19:56 +0000

    Move some functions from bfmodel to bfnode
    Summary:
    
    Test Plan:

--- packages/bfDb/coreModels/BfNode.ts
+++ packages/bfDb/coreModels/BfNode.ts
@@ -1,4 +1,5 @@
 import { BfModel } from "packages/bfDb/classes/BfModel.ts";
+import type { ConnectionArguments, ConnectionInterface } from "relay-runtime";
 import type {
   BfBaseModelMetadata,
   Constructor,
@@ -6,6 +7,9 @@ import type {
 } from "packages/bfDb/classes/BfBaseModelMetadata.ts";
 import { BfEdge } from "packages/bfDb/coreModels/BfEdge.ts";
 import { getLogger } from "deps.ts";
+import { BfCurrentViewer, IBfCurrentViewerInternalAdmin } from "packages/bfDb/classes/BfCurrentViewer.ts";
+import { BfAnyid } from "packages/bfDb/classes/BfBaseModelIdTypes.ts";
+import { bfQueryItemsForGraphQLConnection } from "packages/bfDb/bfDb.ts";
 const logger = getLogger(import.meta);
 
 export type BfNodeRequiredProps = Record<string, unknown>;
@@ -21,6 +25,63 @@ export class BfNode<
   ChildOptionalProps,
   ChildCreationMetadata
 > {
+
+  static async queryConnectionForGraphQL<
+    TThis extends Constructor<
+      BfModel<ChildRequiredProps, ChildOptionalProps, ChildCreationMetadata>
+    >,
+    ChildRequiredProps,
+    ChildOptionalProps,
+    ChildCreationMetadata extends BfNodeCreationMetadata,
+  >(
+    this: TThis,
+    currentViewer: BfCurrentViewer,
+    metadataToQuery: Partial<BfBaseModelMetadata<ChildRequiredProps>>,
+    propsToQuery: Partial<ChildRequiredProps & ChildOptionalProps> = {},
+    connectionArgs: ChildOptionalProps,
+    bfGids: Array<BfAnyid> = [],
+  ): Promise<
+    ConnectionInterface<
+      InstanceType<TThis> & BfBaseModelMetadata<ChildCreationMetadata>
+    > & { count: number }
+  > {
+    const currentViewerIsAdmin = currentViewer instanceof
+      IBfCurrentViewerInternalAdmin;
+    const combinedMetadata = {
+      ...metadataToQuery,
+      // allow internal admins to query all models regardless of owner
+      bfOid: currentViewerIsAdmin
+        ? metadataToQuery.bfOid ?? currentViewer.organizationBfGid
+        : currentViewer.organizationBfGid,
+      className: this.name,
+    };
+    const { edges, ...others } = await bfQueryItemsForGraphQLConnection<
+      ChildRequiredProps & Partial<ChildOptionalProps>,
+      BfBaseModelMetadata<ChildCreationMetadata>
+    >(
+      combinedMetadata,
+      propsToQuery,
+      connectionArgs,
+      bfGids,
+    );
+
+    return {
+      ...others,
+      // @ts-expect-error edge is anytyped but it shouldn't be... it should be a rowitem.
+      edges: edges.map((edge) => ({
+        cursor: edge.cursor,
+        node: new this(
+          currentViewer,
+          edge.node.props,
+          {},
+          edge.node.metadata,
+          true,
+        ).toGraphql(),
+      })),
+    };
+  }
+
+  
   public async createTargetNode<
     TTargetClass extends Constructor<
       BfModel<TProps, Record<string, unknown>, TCreationMetadata>
