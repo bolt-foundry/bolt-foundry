c8a44f9d3ab2b8e22595a5d14b26e9260c8d6aed
Randall Bennett <randall@boltfoundry.com>
2024-09-06 20:19:22 +0000

    Update deps
    Summary:
    
    Test Plan:

--- node_modules/.store/relay-runtime-npm-16.1.0-04839580bb/package/lib/store/experimental-live-resolvers/isLiveStateValue.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-module.exports = function isLiveStateValue(v) {
-  return v != null && typeof v === 'object' && typeof v.read === 'function' && typeof v.subscribe === 'function';
-};
