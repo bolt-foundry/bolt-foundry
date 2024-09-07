5e5f7c81427e1359862b94748db3a3d8c76fff64
Colby Baker <colby@boltfoundry.com>
2024-09-07 02:06:18 +0000

    Marketing Site Responsiveness
    Mostly focuses on behavior of the clip collection as viewport size changes with some other optimizations.

--- packages/client/components/LandingPageClipCollection.tsx
+++ packages/client/components/LandingPageClipCollection.tsx
@@ -3,9 +3,13 @@ import { React } from "deps.ts";
 export function LandingPageClipCollection() {
   return (
     <div className="landing-page-clips-container">
-      <div className="landing-page-clips"></div>
-      <div className="landing-page-clips"></div>
-      <div className="landing-page-clips"></div>
+      <div id="clips-column-one">
+        <div className="landing-page-clips" id="clip1"></div>
+        <div className="landing-page-clips" id="clip2"></div>
+      </div>
+        <div id="clips-column-two">
+          <div className="landing-page-clips" id="clip3"></div>
+        </div>
     </div>
   );
 }
