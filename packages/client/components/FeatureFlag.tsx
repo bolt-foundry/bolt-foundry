7520fca8221234da9a61c3156c22af47b184333b
Justin Carter <justin@boltfoundry.com>
2024-09-09 17:39:05 +0000

    add hud and feature flag
    Summary:
    
    Test Plan:

---
+++ packages/client/components/FeatureFlag.tsx
@@ -0,0 +1,33 @@
+import * as React from "react";
+import { useAppState } from "packages/client/contexts/AppStateContext.tsx";
+import { BfDsToggle } from "packages/bfDs/BfDsToggle.tsx";
+
+type Props = {
+  name: string;
+  toggle?: boolean;
+};
+
+export function FeatureFlag({
+  children,
+  name,
+  toggle,
+}: React.PropsWithChildren<Props>) {
+  const { getFeatureFlag, setFeatureFlag } = useAppState();
+
+  const isFeatureEnabled = getFeatureFlag(name);
+
+  if (toggle) {
+    return (
+      <BfDsToggle
+        label={name}
+        size="small"
+        value={isFeatureEnabled}
+        onChange={() => {
+          setFeatureFlag(name);
+        }}
+      />
+    );
+  }
+
+  return isFeatureEnabled ? children : null;
+}
