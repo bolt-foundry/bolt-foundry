bc3c04ba1917edd28dfece2ce1f40de62f444f4a
Randall Bennett <randall@boltfoundry.com>
2024-09-19 18:37:02 +0000

    Clip search sidebar

--- packages/client/contexts/ClipSearchContext.tsx
+++ packages/client/contexts/ClipSearchContext.tsx
@@ -33,54 +33,54 @@ const ClipSearchContext = createContext<ClipSearchContextProps | undefined>(
 export default function ClipSearchProvider(
   { children }: React.PropsWithChildren,
 ) {
-  const [commit, isInFlight] = useMutation(mutation);
-  const [clips, setClips] = useState<string | null>();
-  const [clipsCount, setClipsCount] = useState<number | null>();
-  const [prompt, setPrompt] = useState("");
-  const [aiModel, setAiModel] = useState(AiModel.OPENAI_4O);
-  const previousPromptRef = React.useRef<string>("");
+  // const [commit, isInFlight] = useMutation(mutation);
+  // const [clips, setClips] = useState<string | null>();
+  // const [clipsCount, setClipsCount] = useState<number | null>();
+  // const [prompt, setPrompt] = useState("");
+  // const [aiModel, setAiModel] = useState(AiModel.OPENAI_4O);
+  // const previousPromptRef = React.useRef<string>("");
 
-  const commitSearch = (input: string = prompt) => {
-    setPrompt(input);
-    setClips(null);
-    setClipsCount(null);
-    commit({
-      variables: {
-        input,
-        suggestedModel: aiModel,
-      },
-      onCompleted: (response) => {
-        setClips(response.searchMutation.message);
-        const parsedClips = isValidJSON(response.searchMutation.message)
-          ? JSON.parse(response.searchMutation.message)
-          : { anecdotes: [] };
-        const numberOfClips = parsedClips?.anecdotes?.length ?? 0;
-        setClipsCount(numberOfClips);
-        previousPromptRef.current = input;
-        setPrompt("");
-      },
-    });
-  };
+  // const commitSearch = (input: string = prompt) => {
+  //   setPrompt(input);
+  //   setClips(null);
+  //   setClipsCount(null);
+  //   commit({
+  //     variables: {
+  //       input,
+  //       suggestedModel: aiModel,
+  //     },
+  //     onCompleted: (response) => {
+  //       setClips(response.searchMutation.message);
+  //       const parsedClips = isValidJSON(response.searchMutation.message)
+  //         ? JSON.parse(response.searchMutation.message)
+  //         : { anecdotes: [] };
+  //       const numberOfClips = parsedClips?.anecdotes?.length ?? 0;
+  //       setClipsCount(numberOfClips);
+  //       previousPromptRef.current = input;
+  //       setPrompt("");
+  //     },
+  //   });
+  // };
 
-  const clearSearch = () => {
-    setPrompt("");
-    setClips(null);
-    setClipsCount(null);
-  };
+  // const clearSearch = () => {
+  //   setPrompt("");
+  //   setClips(null);
+  //   setClipsCount(null);
+  // };
 
   const value = {
-    aiModel,
-    setAiModel,
-    clips,
-    setClips,
-    clipsCount,
-    setClipsCount,
-    clearSearch,
-    commitSearch,
-    isInFlight,
-    prompt,
-    setPrompt,
-    previousPrompt: previousPromptRef.current,
+    // aiModel,
+    // setAiModel,
+    // clips,
+    // setClips,
+    // clipsCount,
+    // setClipsCount,
+    // clearSearch,
+    // commitSearch,
+    // isInFlight,
+    // prompt,
+    // setPrompt,
+    // previousPrompt: previousPromptRef.current,
   };
 
   return (
