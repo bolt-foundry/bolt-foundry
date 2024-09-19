647fae2216cb58d925ee77eed3cd5fbf6545e6a0
Randall Bennett <randall@boltfoundry.com>
2024-09-19 19:38:09 +0000

    Clip search sidebar

--- packages/client/components/clipsearch/Search.tsx
+++ packages/client/components/clipsearch/Search.tsx
@@ -1,14 +1,68 @@
-import type * as React from "react";
+import * as React from "react";
 import { BfDsInput } from "packages/bfDs/BfDsInput.tsx";
 import { DropdownSelector } from "packages/bfDs/DropdownSelector.tsx";
-import { useClipSearchState } from "packages/client/contexts/ClipSearchContext.tsx";
 import { AiModel } from "packages/client/contexts/ClipSearchContext.tsx";
 import { BfDsButton } from "packages/bfDs/BfDsButton.tsx";
 import { FeatureFlag } from "packages/client/components/FeatureFlag.tsx";
+import { graphql } from "packages/client/deps.ts";
+import { useFragment, useMutation } from "react-relay";
+import type { SearchForClipsFragment_bfSearchResultConnection$key } from "packages/__generated__/SearchForClipsFragment_bfSearchResultConnection.graphql.ts";
+import { getLogger } from "deps.ts";
+import type { SearchForClipsMutation } from "packages/__generated__/SearchForClipsMutation.graphql.ts";
+import { useRouter } from "packages/client/contexts/RouterContext.tsx";
 
-export function Search() {
-  const { aiModel, setAiModel, commitSearch, isInFlight, prompt, setPrompt } =
-    useClipSearchState();
+const logger = getLogger(import.meta);
+const mutation = await graphql`
+  mutation SearchForClipsMutation($query: String!, $connections: [ID!]!) {
+    createSearchResult(query: $query) @appendNode(connections: $connections, edgeTypeName: "BfSearchResult") {
+      query
+      id
+    }
+  }
+`;
+
+const fragment = await graphql`
+  fragment SearchForClipsFragment_bfSearchResultConnection on BfSearchResultConnection {
+      __id
+  }
+`;
+
+type Props = {
+  bfSearchResultConnection$key:
+    SearchForClipsFragment_bfSearchResultConnection$key;
+};
+
+export function Search(
+  { bfSearchResultConnection$key }: Props,
+) {
+  const { navigate } = useRouter();
+  const [aiModel, setAiModel] = React.useState(AiModel.OPENAI_4O);
+  const [searchText, setSearchText] = React.useState("");
+  const [commit, isInFlight] = useMutation<SearchForClipsMutation>(mutation);
+  const data = useFragment(
+    fragment,
+    bfSearchResultConnection$key,
+  );
+
+  function commitSearch() {
+    logger.debug("data", data);
+    commit({
+      variables: {
+        query: searchText,
+        connections: [data?.__id],
+      },
+      optimisticResponse: {
+        createSearchResult: {
+          query: `searchText: ${searchText}`,
+          id: "",
+        },
+      },
+      onCompleted: (response) => {
+        response.createSearchResult &&
+          navigate(`/search/${response.createSearchResult.id}`);
+      },
+    });
+  }
 
   function onSubmit(e: React.FormEvent) {
     e.preventDefault();
@@ -21,9 +75,9 @@ export function Search() {
         <BfDsInput
           placeholder="Search"
           showSpinner={isInFlight}
-          value={prompt}
+          value={searchText}
           onChange={(e) => {
-            setPrompt(e.target.value);
+            setSearchText(e.target.value as AiModel);
           }}
         />
       </div>
