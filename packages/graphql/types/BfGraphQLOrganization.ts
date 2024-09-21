ee538d6e828fd9c6fc5fc1499673d5b703dff641
Randall Bennett <randall@boltfoundry.com>
2024-09-21 02:38:12 +0000

    Add savedsearch to org
    Summary:
    
    Test Plan:

--- packages/graphql/types/BfGraphQLOrganization.ts
+++ packages/graphql/types/BfGraphQLOrganization.ts
@@ -1,4 +1,9 @@
 import { objectType } from "packages/graphql/deps.ts";
+import { bfSavedSearchType } from "packages/graphql/types/BfGraphQLSavedSearch.ts";
+import { BfOrganization } from "packages/bfDb/models/BfOrganization.ts";
+import { GraphQLContext } from "packages/graphql/graphql.ts";
+import { toBfGid } from "packages/bfDb/classes/BfBaseModelIdTypes.ts";
+import { BfSavedSearch } from "packages/bfDb/models/BfSavedSearch.ts";
 
 export const BfGraphQLOrganizationType = objectType({
   name: "BfOrganization",
@@ -7,5 +12,19 @@ export const BfGraphQLOrganizationType = objectType({
     t.implements("BfNode");
     t.string("name");
     t.nonNull.id("id");
+    t.connectionField("savedSearches", {
+      type: bfSavedSearchType,
+      resolve: async (
+        { id },
+        args,
+        { bfCurrentViewer }: GraphQLContext,
+      ) => {
+        const person = await BfOrganization.findX(
+          bfCurrentViewer,
+          toBfGid(id),
+        );
+        return person.queryTargetsConnectionForGraphQL(BfSavedSearch, args);
+      },
+    });
   },
 });
