36daad40e61fc3af7e07ae1cbf0d74a17e288e83
Randall Bennett <randall@boltfoundry.com>
2024-09-25 23:36:11 +0000

    Frontend for search
    Summary:
    
    Test Plan:

---
+++ packages/client/components/clipsearch/SearchResults.tsx
@@ -0,0 +1,37 @@
+import { graphql } from "packages/client/deps.ts";
+import { useFragment } from "react-relay";
+import { SearchResults_bfSavedSearch$key } from "packages/__generated__/SearchResults_bfSavedSearch.graphql.ts";
+
+const fragment = await graphql`
+  fragment SearchResults_bfSavedSearch on BfSavedSearch {
+    query
+    searchResults(first: 10) {
+      count
+      edges {
+       node {
+       id
+          body 
+        } 
+      }
+    }
+  }
+`;
+
+type Props = {
+  bfSavedSearch$key?: SearchResults_bfSavedSearch$key;
+};
+
+export function SearchResults({ bfSavedSearch$key }: Props) {
+  const data = useFragment(fragment, bfSavedSearch$key);
+  const list = data?.searchResults?.edges?.map((edge) => {
+    return edge?.node;
+  });
+  const elements = list?.map(({ body, id }) => <div key={id}>{body}</div>);
+  return (
+    <div>
+      <div>query: {data.query}</div>
+      <div>Found {data?.searchResults?.count ?? "no"} results</div>
+      {elements}
+    </div>
+  );
+}
--- packages/graphql/types/BfGraphQLOrganization.ts
+++ packages/graphql/types/BfGraphQLOrganization.ts
@@ -4,6 +4,8 @@ import { BfOrganization } from "packages/bfDb/models/BfOrganization.ts";
 import { GraphQLContext } from "packages/graphql/graphql.ts";
 import { toBfGid } from "packages/bfDb/classes/BfBaseModelIdTypes.ts";
 import { BfSavedSearch } from "packages/bfDb/models/BfSavedSearch.ts";
+import { BfGraphQLCollectionType } from "packages/graphql/types/mod.ts";
+import { BfCollection } from "packages/bfDb/models/BfCollection.ts";
 
 export const BfGraphQLOrganizationType = objectType({
   name: "BfOrganization",
@@ -24,6 +26,21 @@ export const BfGraphQLOrganizationType = objectType({
           toBfGid(id),
         );
         return person.queryTargetsConnectionForGraphQL(BfSavedSearch, args);
+      },
+    });
+    t.connectionField("collections", {
+      type: BfGraphQLCollectionType,
+      async resolve({ id }, args, { bfCurrentViewer }) {
+        const org = await BfOrganization.findX(
+          bfCurrentViewer,
+          toBfGid(id),
+        );
+        const collectionsConnection = await org
+          .queryTargetsConnectionForGraphQL(
+            BfCollection,
+            args,
+          );
+        return collectionsConnection;
       },
     });
   },
