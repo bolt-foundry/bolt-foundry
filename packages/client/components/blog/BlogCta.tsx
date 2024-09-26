8b9108965d1a4b0cf2dde2512f0e57ec8129f66d
Colby Baker <colby@boltfoundry.com>
2024-09-26 23:48:06 +0000

    Improve mobile style
    Removes login button from mobile. (needs hamburger menu, don't want to do that rn, but login isn't needed for mobile anyways)
    Minor changes to mobile layout
    Small refactor of the call to action blog component to pave the way for the new custom text functionality

--- packages/client/components/blog/BlogCta.tsx
+++ packages/client/components/blog/BlogCta.tsx
@@ -1,14 +1,11 @@
 import { BfDsButton } from "packages/bfDs/BfDsButton.tsx";
-import { BlogCTA } from "packages/client/components/blog/BlogCta.tsx";
 
 export function BlogCta() {
   return (
     <div className="blog-page-cta">
       <div className="blog-page-cta-text-area">
-        It's your content, get the most from it.
-        <div className="blog-page-cta-cta">
-          Schedule to see how Bolt Foundry can help!
-        </div>
+        It's your content, get the most from it. Schedule to see how Bolt
+        Foundry can help!
       </div>
       <div className="blog-page-lets-talk-button">
         <BfDsButton
