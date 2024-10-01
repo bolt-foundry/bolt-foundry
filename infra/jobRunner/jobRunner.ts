89b546d71aebed581cb07bffd622996b2388823f
Randall Bennett <randall@boltfoundry.com>
2024-10-01 15:52:17 +0000

    Hack to try to allow for multiple workers without pulling off the same job
    Summary:
    
    Test Plan:

--- infra/jobRunner/jobRunner.ts
+++ infra/jobRunner/jobRunner.ts
@@ -1,4 +1,4 @@
-const WORKER_CONCURRENCY = parseInt(Deno.env.get("WORKER_CONCURRENCY") ?? "1");
+const WORKER_CONCURRENCY = parseInt(Deno.env.get("WORKER_CONCURRENCY") ?? "4");
 import { createWorker } from "packages/packages.ts";
 import { getLogger } from "deps.ts";
 
@@ -7,9 +7,11 @@ const logger = getLogger(import.meta);
 const workers = [];
 
 for (let i = 0; i < WORKER_CONCURRENCY; i++) {
-  const worker = createWorker(
-    import.meta.resolve(`./worker.ts`),
-  );
-  workers.push(worker);
+  setTimeout(() => {
+    const worker = createWorker(
+      import.meta.resolve(`./worker.ts`),
+    );
+    workers.push(worker);
+    logger.info(`Created ${workers.length} job runners`);
+  }, i * 250 + Math.random() * 1000);
 }
-logger.info(`Created ${workers.length} job runners`);
