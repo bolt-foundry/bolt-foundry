673f24aa0095b900f8e5cffcfa1825c760d73841
Randall Bennett <randall@boltfoundry.com>
2024-10-03 16:34:18 +0000

    Add video to graphql for search results
    Summary:
    
    Test Plan:

--- packages/graphql/types/BfGraphQLSavedSearchResult.ts
+++ packages/graphql/types/BfGraphQLSavedSearchResult.ts
@@ -1,6 +1,8 @@
-import { objectType } from "packages/graphql/deps.ts";
+import { objectType, intArg } from "nexus";
 import { BfNodeGraphQLType } from "packages/graphql/types/BfGraphQLNode.ts";
 import { BfGraphQLFakeClipDataType } from "packages/graphql/types/BfGraphQLFakeClipData.ts";
+import { GraphQLVideoDownloadableType, GraphQLVideoPreviewableType } from "packages/graphql/types/GraphQLMedia.ts";
+import { BfSavedSearchResult } from "packages/bfDb/models/BfSavedSearchResult.ts";
 
 export const BfGraphQLSavedSearchResultType = objectType({
   name: "BfSavedSearchResult",
@@ -16,6 +18,20 @@ export const BfGraphQLSavedSearchResultType = objectType({
     t.msTime("duration");
     t.msTime("startTime");
     t.msTime("endTime");
+    t.field("previewable", {
+      type: GraphQLVideoPreviewableType,
+      async resolve({id}, _, { bfCurrentViewer }) {
+        const result = await BfSavedSearchResult.findX(bfCurrentViewer, id);
+        return result.getPreviewableForGraphql();
+      }
+    });
+    t.field("downloadable", {
+      type: GraphQLVideoDownloadableType,
+      async resolve({id}, _, { bfCurrentViewer }) {
+        const result = await BfSavedSearchResult.findX(bfCurrentViewer, id);
+        return result.getDownloadableForGraphql();
+      }
+    });
     t.connectionField("words", {
       type: BfGraphQLFakeClipDataType,
       resolve: (_a, _b, _c) => {
