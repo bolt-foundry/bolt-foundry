a94a4c3ec737c51fc0ae1a8722d45a81fcbc3d9b
Randall Bennett <randall@boltfoundry.com>
2024-10-03 16:34:30 +0000

    Frontend for search results
    Summary:
    
    Test Plan:

--- packages/client/components/clipsearch/DownloadClipButton.tsx
+++ packages/client/components/clipsearch/DownloadClipButton.tsx
@@ -1,33 +1,45 @@
-import type * as React from "react";
-import { useMutation } from "react-relay";
+import { useEffect } from "react";
+import { useFragment, useMutation } from "react-relay";
 import { BfDsButton } from "packages/bfDs/BfDsButton.tsx";
 import { graphql } from "packages/client/deps.ts";
 import { getLogger } from "deps.ts";
 import { sanitizeFilename } from "packages/lib/textUtils.ts";
+import type { DownloadClipButton_bfSavedSearchResult$key } from "packages/__generated__/DownloadClipButton_bfSavedSearchResult.graphql.ts";
 
 const logger = getLogger(import.meta);
 
+const fragment = await graphql`
+  fragment DownloadClipButton_bfSavedSearchResult on BfSavedSearchResult {
+    downloadable {
+      url
+      percentageRendered
+      ready
+    }
+  }
+`;
+
 type Props = {
-  startTime: number;
-  endTime: number;
-  mediaId: string;
-  title: string;
-  transcriptId: string;
-  title: string;
-  disabled: boolean;
+  bfSavedSearchResult$key: DownloadClipButton_bfSavedSearchResult$key;
 };
 
 export function DownloadClipButton(
-  { startTime = 0, endTime = 0, mediaId, title, transcriptId, disabled }: Props,
+  { bfSavedSearchResult$key }: Props,
 ) {
+  const { url, percentageRendered, ready } = useFragment(
+    fragment,
+    bfSavedSearchResult$key,
+  );
+  useEffect(() => {
+    logger.info({ url, percentageRendered, ready });
+  }, [url, percentageRendered, ready]);
+
   const handleDownload = () => {
-    logger.error("Clip download doesn't work yet.");
+    logger.info("download goes here");
   };
   return (
     <BfDsButton
       iconLeft="download"
       onClick={handleDownload}
-      disabled={disabled}
     />
   );
 }
