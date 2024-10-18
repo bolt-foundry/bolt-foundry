8c62f425873b9167dbaade44233b6e6a2590af67
Justin Carter <justin@boltfoundry.com>
2024-10-18 16:11:07 +0000

    move downloadClip from Media to SavedSearchResult
    Summary:
    
    Test Plan:

--- packages/bfDb/models/BfMedia.ts
+++ packages/bfDb/models/BfMedia.ts
@@ -1,15 +1,14 @@
 import { BfNode } from "packages/bfDb/coreModels/BfNode.ts";
 import type { BfCurrentViewer } from "packages/bfDb/classes/BfCurrentViewer.ts";
 import { getLogger } from "deps.ts";
-import type { DGWord } from "packages/types/transcript.ts";
-import { render } from "infra/bff/friends/render.bff.ts";
-import { fetchFile } from "lib/googleDriveApi.ts";
-import { BfPerson } from "packages/bfDb/models/BfPerson.ts";
-import { sanitizeFilename } from "packages/lib/textUtils.ts";
 import type { BfGoogleDriveResource } from "packages/bfDb/models/BfGoogleDriveResource.ts";
 import { BfError } from "lib/BfError.ts";
 import { BfMediaNodeVideoGoogleDriveResource } from "packages/bfDb/models/BfMediaNodeVideoGoogleDriveResource.ts";
-import { BfMediaNodeVideo, BfMediaNodeVideoRole, BfMediaNodeVideoStatus } from "packages/bfDb/models/BfMediaNodeVideo.ts";
+import {
+  BfMediaNodeVideo,
+  BfMediaNodeVideoRole,
+  BfMediaNodeVideoStatus,
+} from "packages/bfDb/models/BfMediaNodeVideo.ts";
 
 const logger = getLogger(import.meta);
 
@@ -20,20 +19,6 @@ export enum GoogleDriveResourceToMediaEdgeRoles {
 export type BfMediaProps = {
   filename: string;
   name: string;
-};
-
-type DownloadClipArgs = {
-  file: {
-    url: string;
-    fileId?: string;
-  };
-  title: string;
-  transcript: {
-    start_time: number;
-    end_time: number;
-    transcript: Array<DGWord>;
-    settings: Record<string, unknown>;
-  };
 };
 
 export class BfMedia extends BfNode<BfMediaProps> {
@@ -119,123 +104,5 @@ export class BfMedia extends BfNode<BfMediaProps> {
       return await target.createTranscript();
     });
     return Promise.all(transcripts);
-  }
-
-  async downloadClip(args: DownloadClipArgs) {
-    const {
-      file: {
-        url,
-        // "short video.mp4"
-        fileId = "1JOvVaUjfMOzhvzkztbFyII6Oe7Qu9Hrh",
-      },
-      transcript,
-      title,
-    } = args;
-    const fileroot = await Deno.makeTempFile();
-    const transcriptFilename = `${fileroot}.json`;
-    const testVideoFilename = `${fileroot}.mp4`;
-    // HACK FOR DEMO, LIMIT TO 20 SECONDS
-    // because replit runs out of disk space
-    const transcriptOverride = {
-      ...transcript,
-      end_time: transcript.start_time + 10,
-    };
-    const fileContent = JSON.stringify(transcriptOverride, null, 2);
-    try {
-      await Deno.writeTextFile(transcriptFilename, fileContent);
-      logger.info(`Transcript file created at ${transcriptFilename}`);
-    } catch (error) {
-      logger.error(
-        `Failed to create transcript file at ${transcriptFilename}: ${error}`,
-      );
-      throw new Error(error);
-    }
-
-    // Download and save the file from Google Drive
-    logger.info("File url", url);
-
-    try {
-      const currentViewerPerson = await BfPerson.findCurrentViewer(
-        this.currentViewer,
-      );
-      const googleAuth = await currentViewerPerson?.getGoogleAuth();
-      if (!googleAuth) {
-        throw new Error("no google auth");
-      }
-      const accessToken = await googleAuth.getAccessToken();
-      const response = await fetchFile(accessToken, fileId);
-      if (!response.ok) {
-        throw new Error(
-          `Failed to fetch file from Google Drive: ${response.statusText}`,
-        );
-      }
-      const fileData = new Uint8Array(await response.arrayBuffer());
-      await Deno.writeFile(testVideoFilename, fileData);
-      logger.info(
-        `File downloaded from Google Drive and saved to ${testVideoFilename}`,
-      );
-    } catch (error) {
-      logger.error(`Failed to download file from Google Drive: ${error}`);
-      throw new Error(error);
-    }
-
-    const renderCode = await render(["-i", testVideoFilename]);
-
-    const extension = testVideoFilename.split(".").pop();
-    const renderedFilename = testVideoFilename.replace(
-      new RegExp(`\.${extension}$`),
-      `_render.${extension}`,
-    );
-
-    // Hack for demo
-    // Copy file from renderedFilename to {BF_ROOT}/build/downloads
-    const formattedTitle = title
-      ? `${sanitizeFilename(title)}.mp4`
-      : renderedFilename.split("/").pop();
-    const BFF_ROOT = Deno.env.get("BFF_ROOT") ?? Deno.cwd();
-    const destinationPath = `${BFF_ROOT}/build/downloads/${formattedTitle}`;
-    try {
-      await Deno.copyFile(renderedFilename, destinationPath);
-      logger.info(`File copied to ${destinationPath}`);
-    } catch (error) {
-      logger.error(`Failed to copy file to ${destinationPath}: ${error}`);
-      throw new Error(error);
-    }
-
-    // Clear /tmp folder
-    logger.info("Clearing /tmp folder");
-    await Deno.remove(fileroot);
-    logger.info("❌", fileroot);
-    await Deno.remove(testVideoFilename);
-    logger.info("❌", testVideoFilename);
-    await Deno.remove(transcriptFilename);
-    logger.info("❌", transcriptFilename);
-    await Deno.remove(renderedFilename);
-    logger.info("❌", renderedFilename);
-
-    for await (const dirEntry of Deno.readDir("/tmp")) {
-      const entryPath = `/tmp/${dirEntry.name}`;
-      if (dirEntry.isFile) {
-        if (
-          dirEntry.name.includes("vcscut") ||
-          dirEntry.name.includes("cut_ffmpegout")
-        ) {
-          await Deno.remove(entryPath);
-          logger.info("❌", entryPath);
-        }
-      } else if (dirEntry.isDirectory) {
-        if (dirEntry.name.includes("vcs")) {
-          await Deno.remove(entryPath, { recursive: true });
-          logger.info("❌", entryPath);
-        }
-      }
-    }
-    logger.info("Cleared /tmp folder");
-
-    if (renderCode !== 0) {
-      logger.error(`Error rendering ${renderedFilename}`);
-      throw new Error(`Error rendering ${renderedFilename}`);
-    }
-    logger.info(`Rendered ${renderedFilename}`);
   }
 }
