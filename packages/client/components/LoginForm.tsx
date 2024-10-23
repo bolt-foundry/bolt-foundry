7449f5053f2e59203b6e6c5a74b852993a00c773
Randall Bennett <randall@boltfoundry.com>
2024-10-23 20:58:15 +0000

    Fix a google login race condition
    Summary:
    
    Test Plan:

--- packages/client/components/LoginForm.tsx
+++ packages/client/components/LoginForm.tsx
@@ -1,9 +1,10 @@
+/// <reference types="@types/google.accounts" />
 import { graphql } from "packages/client/deps.ts";
 import { useAppEnvironment } from "packages/client/contexts/AppEnvironmentContext.tsx";
 import { useRouter } from "packages/client/contexts/RouterContext.tsx";
 import { getLogger } from "packages/logger/logger.ts";
 const logger = getLogger(import.meta);
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import { useMutation } from "react-relay";
 
@@ -25,6 +26,7 @@ const loginWithGoogleMutation = await graphql`
 function GoogleLoginButton() {
   const { navigate } = useRouter();
   const [commit] = useMutation(loginWithGoogleMutation);
+  const [googleLibraryLoaded, setGoogleLibraryLoaded] = useState(false);
 
   const googleSignInButtonRef = useRef(null);
   const { GOOGLE_OAUTH_CLIENT_ID } = useAppEnvironment();
@@ -67,25 +69,39 @@ function GoogleLoginButton() {
   };
 
   useEffect(() => {
-    google.accounts.id.initialize({
-      client_id: GOOGLE_OAUTH_CLIENT_ID,
-      use_fedcm_for_prompt: true,
-      auto_select: true,
-      callback: onLogin,
-    });
-
-    if (googleSignInButtonRef.current) {
-      google.accounts.id.renderButton(
-        googleSignInButtonRef.current,
-        {
-          theme: "outline",
-          text: "continue_with",
-          size: "large",
-          type: "standard",
-        },
-      );
+    if (google) {
+      setGoogleLibraryLoaded(true);
+    } else {
+      // @ts-expect-error Not typed.
+      globalThis.onGoogleLibraryLoad = () => {
+        setGoogleLibraryLoaded(true);
+      };
     }
   }, []);
+
+  useEffect(() => {
+    if (googleLibraryLoaded) {
+      google.accounts.id.initialize({
+        client_id: GOOGLE_OAUTH_CLIENT_ID,
+        use_fedcm_for_prompt: true,
+        auto_select: true,
+        callback: onLogin,
+      });
+
+      if (googleSignInButtonRef.current) {
+        google.accounts.id.renderButton(
+          googleSignInButtonRef.current,
+          {
+            theme: "outline",
+            text: "continue_with",
+            size: "large",
+            type: "standard",
+          },
+        );
+      }
+    }
+    
+  }, [googleLibraryLoaded]);
 
   return <div ref={googleSignInButtonRef} style={{ marginTop: 20 }}></div>;
 }
