4de24ad9598068b417f8a0e8e62d2bd0b5894fe7
Randall Bennett <randallb@noreply.githubusers.com>
2025-03-03 16:03:19 +0000

    BfNodeOnDisk v0
    Summary:
    
    Test Plan:

---
+++ packages/bfDb/classes/BfNodeOnDisk.ts
@@ -0,0 +1,20 @@
+import {
+  type BfMetadataBase,
+  BfNodeBase,
+  type BfNodeBaseProps,
+} from "packages/bfDb/classes/BfNodeBase.ts";
+import type { BfCurrentViewer } from "packages/bfDb/classes/BfCurrentViewer.ts";
+import type { BfGid } from "packages/bfDb/classes/BfNodeIds.ts";
+import { getLogger } from "packages/logger.ts";
+
+const _logger = getLogger(import.meta);
+
+/**
+ * BfNodeOnDisk - A node implementation that persists data to disk
+ */
+export class BfNodeOnDisk<
+  TProps extends BfNodeBaseProps = BfNodeBaseProps,
+  TMetadata extends BfMetadataBase = BfMetadataBase,
+> extends BfNodeBase<TProps, TMetadata> {
+  // Implementation will be added as we make the tests pass
+}
