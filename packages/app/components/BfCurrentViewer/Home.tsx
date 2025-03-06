fe7049a9df3bdea208193e1e93697068d56c9a6d
runner@a3b1b52afd4f <>
2025-03-06 23:36:28 +0000

    bug fix for home page button
    Summary:
    
    Test Plan:

--- packages/app/components/BfCurrentViewer/Home.tsx
+++ packages/app/components/BfCurrentViewer/Home.tsx
@@ -1,6 +1,9 @@
 import { iso } from "packages/app/__generated__/__isograph/iso.ts";
 import { CfLogo } from "packages/app/resources/CfLogo.tsx";
 import { useFeatureFlagEnabled } from "packages/app/hooks/useFeatureFlagHooks.ts";
+import { useRouter } from "packages/app/contexts/RouterContext.tsx";
+import { BfDsButton } from "packages/bfDs/components/BfDsButton.tsx";
+
 export const Home = iso(`
   field BfCurrentViewer.Home @component {
     __typename
@@ -13,6 +16,7 @@ export const Home = iso(`
   }
 `)(function Home({ data }) {
   // Extract content items from the data
+  const { navigate } = useRouter();
   const demoButton = data?.asBfCurrentViewerLoggedOut?.DemoButton;
   const collection = data?.contentCollection;
   const showExtendedContent = useFeatureFlagEnabled("show_extended_content");
@@ -32,7 +36,16 @@ export const Home = iso(`
         </div>
 
         <div className="loginBox">
-          {demoButton && <data.asBfCurrentViewerLoggedOut.DemoButton />}
+          {demoButton
+            ? <data.asBfCurrentViewerLoggedOut.DemoButton />
+            : (
+              <BfDsButton
+                kind="primary"
+                text="Login"
+                onClick={() => navigate("/login")}
+              />
+            )}
+
           {collection && showExtendedContent
             ? <data.contentCollection.ContentCollection />
             : <p>Coming soon.</p>}
