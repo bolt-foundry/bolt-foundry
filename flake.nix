3efc3e0ff1249d09bd0f6270b1ef44cb7cf24570
runner@ac9752ba4ac3 <>
2025-03-10 15:02:09 +0000

    flake update
    Summary:
    
    Test Plan:

--- flake.nix
+++ flake.nix
@@ -1,6 +1,5 @@
-
 {
-  description = "Nix flake referencing replit.nix";
+  description = "Nix flake referencing replit.nix with symlink filtering";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
@@ -10,14 +9,20 @@
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
-      replit = import ./replit.nix { inherit pkgs; };
+
+      filteredSrc = pkgs.lib.cleanSourceWith {
+        src = ./.;
+        filter = path: type: !(type == "symlink" && builtins.readDir path != {});
+      };
+
+      replit = import (filteredSrc + "/replit.nix") { inherit pkgs; };
+
     in
     {
       devShells.${system}.default = pkgs.mkShell {
         buildInputs = [ pkgs.git ] ++ replit.deps;
         shellHook = ''
           deno install
-          
         '';
       };
     };
