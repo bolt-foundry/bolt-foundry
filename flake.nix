0e494a5de80f674abd4a296de53414a6e747cb02
Randall Bennett <randall@boltfoundry.com>
2025-03-11 12:25:02 -0400

    Update flake for local
    Summary:
    
    Test Plan:

--- flake.nix
+++ flake.nix
@@ -1,24 +1,24 @@
-
 {
   description = "Nix flake referencing replit.nix";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
   };
-
-  outputs = { self, nixpkgs, ... }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
-      replit = import ./replit.nix { inherit pkgs; };
-    in
-    {
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = [ pkgs.git ] ++ replit.deps;
-        shellHook = ''
-          deno install
-          
-        '';
-      };
-    };
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        replit = import ./replit.nix { 
+          inherit pkgs system; 
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [ pkgs.git ] ++ replit.deps;
+          shellHook = ''
+            deno install
+          '';
+        };
+      }
+    );
 }
