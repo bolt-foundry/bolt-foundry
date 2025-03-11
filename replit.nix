c1ae2a703b5ab0c8b19b6567eec2c4f99b993496
runner@e396661ba50e <>
2025-03-11 16:37:40 +0000

    Hide dotfiles in collections
    Summary:
    
    Test Plan:

--- replit.nix
+++ replit.nix
@@ -1,9 +1,9 @@
-{ pkgs }:
+{ pkgs, system ? builtins.currentSystem }:
 let
   unstablePkgs = import (builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/482ffc541c8ffb8d2d09018dba09667d8d2e5e7f.tar.gz";
     sha256 = "1wvj6dl9y5gp3xb8vqlg625xi0ci4gj9zmg52qmz0hyj4rgh1mv7";
-  }) { };
+  }) { inherit system; };
 in
 {
   deps = [
