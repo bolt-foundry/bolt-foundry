9a8e9a047ce1f9f113c9501b1ca96c3f0387f3e4
runner@2c03312770b6 <>
2025-03-12 14:14:32 +0000

    Refactor part 3
    Summary:
    
    Test Plan:

---
+++ packages/web/handlers/domainHandler.ts
@@ -0,0 +1,33 @@
+import { getLogger } from "packages/logger.ts";
+import type { Handler } from "packages/web/web.tsx";
+import { matchRoute } from "./requestHandler.ts";
+
+const logger = getLogger(import.meta);
+
+/**
+ * Handles domain-specific routing
+ */
+export async function handleDomains(
+  req: Request,
+  routes: Map<string, Handler>,
+): Promise<Response | null> {
+  const reqUrl = new URL(req.url);
+  const domain = Deno.env.get("SERVE_PROJECT") ?? reqUrl.hostname;
+
+  if (domain === "biglittletech.ai") {
+    const pathWithParams = `/biglittletech.ai`;
+    logger.setLevel(logger.levels.DEBUG);
+    logger.debug(routes, routes.get(pathWithParams));
+    logger.resetLevel();
+
+    const [handler, routeParams] = matchRoute(
+      pathWithParams,
+      routes,
+      () => new Response("Not found™", { status: 404 }),
+    );
+
+    return await handler(req, routeParams);
+  }
+
+  return null;
+}
