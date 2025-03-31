3f5fd457f82047cdcb00b90d27e9ba9d708b0e70
runner@71c61b257fe5 <>
2025-03-31 21:29:16 +0000

    next
    Summary:
    
    Test Plan:

---
+++ thirdParty/nextjs-sample/pages/_app.tsx
@@ -0,0 +1,35 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
+import { Router } from 'next/router'
+import posthog from 'posthog-js'
+import { PostHogProvider } from 'posthog-js/react'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+      api_host: '/ingest',
+      ui_host: 'https://us.posthog.com',
+      loaded: (posthogInstance) => {
+        if (process.env.NODE_ENV === 'development') {
+          posthogInstance.debug()
+        }
+      },
+    })
+
+    const handleRouteChange = () => posthog?.capture('$pageview')
+    Router.events.on('routeChangeComplete', handleRouteChange)
+
+    return () => {
+      Router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [])
+
+  return (
+    <PostHogProvider client={posthog}>
+      <Component {...pageProps} />
+    </PostHogProvider>
+  )
+}
+
+export default MyApp
