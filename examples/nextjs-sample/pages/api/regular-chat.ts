f351d863ea71d47330ed7b0957890ebffdf42545
Randall Bennett <randallb@users.noreply.github.com>
2025-04-16 14:03:35 +0000

    Rename secret
    Summary:
    
    Test Plan:

--- examples/nextjs-sample/pages/api/regular-chat.ts
+++ examples/nextjs-sample/pages/api/regular-chat.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { connectBoltFoundry } from '@bolt-foundry/bolt-foundry-next';
 import OpenAI from "openai";
 const client = new OpenAI({
-  fetch: connectBoltFoundry(process.env.POSTHOG_NEXTJS_API_KEY)
+  fetch: connectBoltFoundry(process.env.EXAMPLES_NEXTJS_SAMPLE_POSTHOG_NEXTJS_API_KEY)
 });
 
 
