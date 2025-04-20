04495ef876559ad44899be5fa73a491b6eca409b
Randall Bennett <randallb@users.noreply.github.com>
2025-04-20 16:38:29 +0000

    Dead code society
    Summary:
    
    Test Plan:

--- apps/boltFoundry/__generated__/__isograph/BfCurrentViewerLoggedOut/LoginButton/param_type.ts
+++
@@ -1,7 +0,0 @@
-
-export type BfCurrentViewerLoggedOut__LoginButton__param = {
-  readonly data: {
-    readonly __typename: string,
-  },
-  readonly parameters: Record<PropertyKey, never>,
-};
--- apps/boltFoundry/__generated__/__isograph/BfCurrentViewerLoggedOut/RegisterButton/output_type.ts
+++
@@ -1,4 +0,0 @@
-import type { ExtractSecondParam, CombineWithIntrinsicAttributes } from '@isograph/react';
-import type React from 'react';
-import { RegisterButton as resolver } from '../../../../components/BfCurrentViewer/BfCurrentViewerLoggedOut/RegisterButton.tsx';
-export type BfCurrentViewerLoggedOut__RegisterButton__output_type = (React.FC<CombineWithIntrinsicAttributes<ExtractSecondParam<typeof resolver>>>);
