aa2ad6f222935a91abf754486ddacb63def80ef4
Randall Bennett <randall@contentfoundry.com>
2025-06-16 11:11:00 -0400

    chore: update aibff build timestamp

--- apps/aibff/version.ts
+++ apps/aibff/version.ts
@@ -2,5 +2,5 @@
 export const VERSION = "0.1.0-dev";
 
 // Build metadata - will be replaced during build
-export const BUILD_TIME = "2025-06-13T22:19:31.152Z";
+export const BUILD_TIME = "2025-06-16T13:45:22.590Z";
 export const BUILD_COMMIT = "";
